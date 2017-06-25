37b0df09bd31f87226ebf62b00a61d31e5da0dbc
manoharthorat <manoharthorat725@gmail.com>
2017-06-25 16:55:46 +0530

    Single Circular Linked list

--- LinkedList/SCLL.c
+++ LinkedList/SCLL.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
-
+#include<string.h>
 struct node 
 {
 	int data;
 	struct node * next;
 };
 
-int InsertFirst(struct node ** head, int no)
+int InsertFirst(struct node ** head, struct node ** tail, int no)
 {
-	struct node * newonde = (struct node * )malloc(sizeof(struct node ));
+	struct node * newnode = (struct node * )malloc(sizeof(struct node ));
 	if(newnode ==NULL)
 	{
 		return 0;
@@ -17,17 +17,53 @@ int InsertFirst(struct node ** head, int no)
 	memset(newnode,0, sizeof(struct node ));
 	newnode -> next=NULL;
 	newnode -> data=no;
-	if(*head ==NULL)
+	if(*head ==NULL && *tail ==NULL)
 	{
 		*head=newnode;
+		*tail=newnode;
+		(*tail)->next=*head;
+		return 0;
 	}
+	else
+	{
+		newnode -> next=*head;
+		*head=newnode;
+		(*tail)->next=newnode;
+	}
+return 0;
 }
-void Display(struct node *head)
+void Display(struct node *head, struct node * tail)
 {
+	do{
+		printf("%d ",head->data);
+		head = head->next;
+	}while(head != tail->next);
 }
 
-int InsertLast(struct node **head, int no)
+int InsertLast(struct node **head, struct node ** tail, int no)
 {
+	struct node * newnode = (struct node * ) malloc (sizeof(struct node ));
+	if(newnode ==NULL)
+	{
+		return 0;
+	}
+	memset(newnode , 0, sizeof(struct node));
+	newnode-> next = NULL;
+	newnode->data = no;
+	if(*head==NULL && *tail ==NULL)
+	{
+		*head=newnode;
+		*tail=newnode;
+		(*tail)->next =*head;
+		return 0;
+	}
+	else
+	{
+		(*tail)->next= newnode;
+		(*tail)=newnode;
+		(*tail)->next=*head;	
+	}
+return 0;	
 }
 int CountNOde(struct node * head)
 {
@@ -35,7 +71,7 @@ int CountNOde(struct node * head)
 int InsertAtPosition(struct node ** head , int pos, int no)
 {
 }
-int SearchFirst(struct ndoe * head)
+int SearchFirst(struct node * head)
 {
 }
 
@@ -72,6 +108,18 @@ int ReverseList(struct node * head)
 int main()
 {
 struct node * first=NULL;
+struct node * last=NULL;
+InsertFirst(&first,&last,10);
+InsertFirst(&first,&last,20);
+InsertFirst(&first,&last,30);
+InsertFirst(&first,&last,40);
+InsertFirst(&first,&last,50);
+
+Display(first,last);
+InsertLast(&first, &last, 60);
+printf("\n");
+Display(first,last);
+
 
 return 0;	
 }
